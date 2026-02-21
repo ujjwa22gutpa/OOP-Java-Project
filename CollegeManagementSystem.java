2ad052ce58cf40102626e94205c498e9e93be590
Ujjwal <ujjwa22gupta>
2026-02-21 23:43:18 +0530

    Refactor College and CollegeManagementSystem classes to implement constructor and improve instantiation

--- CollegeManagementSystem.java
+++ CollegeManagementSystem.java
@@ -1,47 +1,59 @@
-class College{
+class College {
     private String name;
     private int rollNo;
     private int marks;
 
-
-    void setName( String name){
-        this.name=name;
+    College(String name, int rollNo, int marks) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.marks = marks;
     }
 
-    String getName(){
+    // void setName( String name){
+    // this.name=name;
+    // }
+    String getName() {
         return name;
     }
-    void setRollNo(int rollNo){
-        if(rollNo<0){
-            System.out.println("Invalid roll number");
-            return;
-        }else  this.rollNo=rollNo;
-       
-    }
-    int getRollNo(){
+    // void setRollNo(int rollNo){
+    // if(rollNo<0){
+    // System.out.println("Invalid roll number");
+    // return;
+    // }else this.rollNo=rollNo;
+
+    // }
+    int getRollNo() {
         return rollNo;
     }
 
-    void setMarks( int marks){
-        if(marks<0 || marks >100){
-            System.out.println("plase enter the valid marks ");
-            return;
-        }else this.marks=marks;
-    }
-
-    int getMarks(){
+    // void setMarks( int marks){
+    // if(marks<0 || marks >100){
+    // System.out.println("plase enter the valid marks ");
+    // return;
+    // }else this.marks=marks;
+    // }
+    int getMarks() {
         return marks;
     }
+    // int [] getEverything (){
+    // return new int[] {name,};
+    // }
 }
 
-class CollegeManagementSystem{
+class CollegeManagementSystem {
     public static void main(String[] args) {
-        College s1=new College();
-        s1.setName("ujjwal");
-         System.out.println(s1.getName());
-         s1.setRollNo(2207190);
-         System.out.println(s1.getRollNo());
-         s1.setMarks(99);
-         System.out.println(s1.getMarks());
+        // College s1=new College();
+        // s1.setName("ujjwal");
+        // System.out.println(s1.getName());
+        // s1.setRollNo(2207190);
+        // System.out.println(s1.getRollNo());
+        // s1.setMarks(99);
+        // System.out.println(s1.getMarks());
+
+        College s1 = new College("ujjwal", 2207100, 99);
+        College s2 = new College("Krishna", 14466456, 98);
+        System.out.println(s1.getMarks() + "  " + s1.getName() + "   " + s1.getRollNo());
+        System.out.println(s2.getMarks() + "  " + s2.getName() + "   " + s2.getRollNo());
+
     }
 }
